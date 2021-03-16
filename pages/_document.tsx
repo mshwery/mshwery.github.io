b0ddca9b6b932452a21babbf58d49ef0b6b24bce
Matt Shwery <mshwery@gmail.com>
2021-03-16 15:49:16 -0500

    add DM Sans google font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,11 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta name="theme-color" content="#ffffff" />
+          
+          {/* Google Fonts */}
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet" />
+
           {/* Google Analytics */}
           <script dangerouslySetInnerHTML={{ __html: `
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
