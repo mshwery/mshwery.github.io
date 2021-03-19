6ef5165de2d09b9d6728cdb0c327509fadbd5074
Matt Shwery <mshwery@gmail.com>
2021-03-19 10:52:34 -0500

    use async GA script and track SPA pageviews

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
+import * as React from 'react'
+import { useRouter } from 'next/router'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  React.useEffect(() => {
+    const handleRouteChange = (path: string) => {
+      if (typeof ga !== 'function') return
+      ga('set', 'page', path)
+      ga('send', 'pageview')
+    }
+
+    router.events.on("routeChangeComplete", handleRouteChange)
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,16 +14,15 @@ class MyDocument extends Document {
 
           {/* Google Analytics */}
           {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_PROPERTY_ID && (
-            <script dangerouslySetInnerHTML={{ __html: `
-              (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-              })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-            
-              ga('create', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_PROPERTY_ID}', 'auto');
-              ga('send', 'pageview');
-            ` }}>
-            </script>
+            <>
+              <script dangerouslySetInnerHTML={{ __html: `
+                window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;              
+                ga('create', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_PROPERTY_ID}', 'auto');
+                ga('send', 'pageview');
+              ` }}>
+              </script>
+              <script async src='https://www.google-analytics.com/analytics.js'></script>
+            </>
           )}
         </Head>
         <body>
